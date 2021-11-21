7ddd7703ac25a26eb7f53116d7f50604f633fda5
prpr48 <prpr48@naver.com>
2021-11-21 12:39:27 +0900

    use core service in api application

--- fpms-api/src/main/java/org/example/fpms/api/FpmsApiApplication.java
+++ fpms-api/src/main/java/org/example/fpms/api/FpmsApiApplication.java
@@ -1,5 +1,6 @@
 package org.example.fpms.api;
 
+import org.example.fpms.core.service.CoreService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class FpmsApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(FpmsApiApplication.class, args);
+    CoreService coreService = new CoreService();
+    coreService.hello();
   }
 
 }
