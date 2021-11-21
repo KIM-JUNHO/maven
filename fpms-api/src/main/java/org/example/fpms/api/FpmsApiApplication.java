fdb482395d5e57f048e188adbcda0fc9e3ab1be0
prpr48 <prpr48@naver.com>
2021-11-21 12:28:12 +0900

    add spring boot application

---
+++ fpms-api/src/main/java/org/example/fpms/api/FpmsApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.fpms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpmsApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FpmsApiApplication.class, args);
+  }
+
+}
